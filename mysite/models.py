207d4dacf6dafdec78b8964409b95fb451825d66
Tomvictor <vjtomvictor@icloud.com>
2016-07-21 14:46:30 +0530

    file uploading working perfectly

--- mysite/models.py
+++ mysite/models.py
@@ -5,3 +5,5 @@ from django.db import models
 
 class drive(models.Model):
     file = models.FileField(upload_to="drive")
+    def __str__(self):
+        return self.file.name
--- mysite/views.py
+++ mysite/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .forms import UploadForm
-
+from .models import drive
 # Create your views here.
 
 
 def home(request):
+    if request.method == 'POST':
+        form = UploadForm(request.POST,request.FILES)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            print(instance)
+            instance.save()
+
     context = {
         "form":UploadForm
     }
